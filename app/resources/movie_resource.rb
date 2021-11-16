9d29212c0a6204214dea69aa895fdbc3ca485a1a
anonymous anonymous <taylor.james@kellogg.northwestern.edu>
2021-11-16 03:42:11 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,16 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :year, :integer
+  attribute :duration, :integer
+  attribute :description, :string
+  attribute :image, :string
+  attribute :director_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end

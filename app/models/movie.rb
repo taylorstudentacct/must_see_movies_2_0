0563cf006aba7b990aa9bb72b3d2f9eaf1301327
anonymous anonymous <taylor.james@kellogg.northwestern.edu>
2021-11-16 03:41:20 +0000

    Direct association between movies and reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ApplicationRecord
   # Direct associations
 
+  has_many   :reviews,
+             :dependent => :destroy
+
   has_many   :roles,
              :dependent => :destroy
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  belongs_to :movie
+
   # Indirect associations
 
   # Validations
